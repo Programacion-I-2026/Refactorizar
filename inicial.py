b00726cc848992eb9ab8861978ab94c8da2d81c1
manuel figueroa <manuel.figue06@gmail.com>
2026-04-29 11:11:18 -0300

    nivel basico

--- inicial.py
+++ inicial.py
@@ -1,15 +1,20 @@
 opcion = 0
 saldo = 10000
-
-while opcion != 4:
+datosconsumidos=50000
+while opcion != 5:
 
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Consultar saldo")
     print("2. Ver promociones")
     print("3. Recargar saldo")
     print("4. Salir")
+    print("5.consultar datos consumidos")
 
-    opcion = int(input("Seleccione una opción: "))
+    try:
+         opcion = int(input("Seleccione una opción: "))
+    except ValueError:
+        print("Error: ingresa un numero valido")
+        continue
 
     if opcion == 1:
         print("\nSu saldo actual es: $", saldo)
@@ -26,6 +31,8 @@ while opcion != 4:
 
     elif opcion == 4:
         print("\nGracias por comunicarse. ¡Hasta luego!")
+    elif opcion == 5:
+        print("\nlos datos consumidos son",datosconsumidos)
 
     else:
         print("Opción inválida. Intente nuevamente.")
