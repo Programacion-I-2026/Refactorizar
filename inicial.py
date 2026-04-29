af91ab5cced1476d38a2e069ee0b809403965341
martin peralta <martinperalta2593@gmail.com>
2026-04-29 11:31:04 -0300

    Nivel interesante

--- inicial.py
+++ inicial.py
@@ -1,6 +1,7 @@
 opcion = 0
 saldo = 10000
 datoConsumido= 50000
+contador = 0
 
 while opcion != 4:
 
@@ -15,25 +16,31 @@ while opcion != 4:
 
     if opcion == 1:
         print("\nSu saldo actual es: $", saldo)
+        contador +=1
 
     elif opcion == 2:
         print("\nPromociones disponibles:")
         print("- 2x1 en recargas")
         print("- 30% de descuento en datos móviles")
+        contador +=1
 
     elif opcion == 3:
         monto = int(input("Ingrese monto a recargar: "))
         if monto > 0:
             saldo = saldo + monto
             print("Recarga exitosa. Nuevo saldo: $", saldo)
+            contador +=1
         else:
             print("Ingrese un saldo positivo porfavor.")
 
     elif opcion == 4:
         print("\nGracias por comunicarse. ¡Hasta luego!")
+        contador +=1
 
     elif opcion == 5:
         print("\nLos datos consumidos son", datoConsumido)
+        contador +=1
 
     else:
         print("Opción inválida. Intente nuevamente.")
+print(f"\n usted realizo: {contador} operaciones.")
