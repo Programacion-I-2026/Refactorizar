cf2bc4a363ca647e07b65a21b2d5746d0751ef50
Teromax34 <teromax34@gmail.com>
2026-04-29 16:15:14 -0300

    agregado la huevada que muestra las operaciones realizadas

--- inicial.py
+++ inicial.py
@@ -1,8 +1,9 @@
 opcion = 0
 saldo = 10000
 datosCons = 500
+operaciones = 0
 
-while opcion != 4:
+while opcion != 5:
 
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Consultar saldo")
@@ -15,12 +16,15 @@ while opcion != 4:
 
     match opcion:
         case 1:
+            operaciones += 1
             print("\nSu saldo actual es: $", saldo)
         case 2:
+            operaciones += 1
             print("\nPromociones disponibles:")
             print("- 2x1 en recargas")
             print("- 30% de descuento en datos móviles")
         case 3:
+            operaciones += 1
             monto = int(input("Ingrese monto a recargar: "))
             if monto > 0:
                 saldo = saldo + monto
@@ -28,9 +32,12 @@ while opcion != 4:
             else:
                 print("porfavor ingrese un numero positivo")
         case 4:
+            operaciones += 1
             print("\nActualmente consumiste", datosCons, "datos") 
         case 5:
+            operaciones += 1
             print("\nGracias por comunicarse. ¡Hasta luego!") 
+            print("\nCantidad de operaciones", operaciones)
         case _:
             print("Opción inválida. Intente nuevamente.")
 
