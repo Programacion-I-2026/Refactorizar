58f8a799da82db46c672363d051004d459b47083
Juan <juancinta2@gmail.com>
2026-04-29 19:40:01 -0300

    Nivel interesante

--- inicial.py
+++ inicial.py
@@ -1,18 +1,16 @@
 opcion = 0
 saldo = 10000
 dat_consu = 100
+cont = 0
 
-while opcion != 4:
-
+while opcion != 5:
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Consultar saldo")
     print("2. Ver promociones")
     print("3. Recargar saldo")
     print("4. Consultar datos consumidos")
-    print("5. Salir")
-
+    print("5. Salir") 
     opcion = int(input("Seleccione una opción: "))
-
     if opcion == 1:
         print("\nSu saldo actual es: $", saldo)
 
@@ -31,9 +29,11 @@ while opcion != 4:
     
     elif opcion == 4:
         print("Datos consumidos:", dat_consu)
-
+        
     elif opcion == 5:
         print("\nGracias por comunicarse. ¡Hasta luego!")
-
     else:
         print("Opción inválida. Intente nuevamente.")
+    cont = cont + 1
+    
+print(f"Hiciste: {cont} operaciones")
